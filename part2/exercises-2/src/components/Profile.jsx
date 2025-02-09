8dd448c3155b07d2f318fbc2e6f95839e1ecd419
CodyA1117 <adamscody079@gmail.com>
2025-02-09 07:30:05 -0600

    updated and organized code adding notes, and button to div adjusting the conditional.

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -2,25 +2,27 @@ import './styles.css';
 import Button from './Button';
 import oceans from './oceans.json';
 
-
+// Map through the oceans JSON and create a list of profile components
 const listItem = oceans.map(ocean => (
-   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+   <div key={ocean.id} className={`${ocean.fishCheck ? "isAFish" : "profile"}`}>
       <h1>{ocean.name}</h1>
+      <h3>Fun Facts:</h3>
+      <ul>
+         <li>{ocean.fact1}</li>
+         <li>{ocean.fact2}</li>
+         <li>{ocean.fact3}</li>
+      </ul>
       <img src={ocean.image} alt={ocean.name} className="img" />
+      {/* Button component inside the profile */}
+      <Button />
    </div>
 ));
 
-
-
-function Profile()
-{
-   return(
-      <>
-         <ul>
-            {listItem}
-         </ul>
-         <Button />
-      </>  
+function Profile() {
+   return (
+      <div>
+         {listItem}
+      </div>
    );
 }
 
